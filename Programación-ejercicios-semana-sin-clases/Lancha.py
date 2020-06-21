7178c7e02d800b0a55f6f0b763596bdfa2d0e095
Kevin Figueroa <64949206+DrKoi@users.noreply.github.com>
2020-06-21 15:09:45 -0400

    jaasila

--- Programación-ejercicios-semana-sin-clases/Lancha.py
+++ Programación-ejercicios-semana-sin-clases/Lancha.py
@@ -2,9 +2,9 @@ def DefinirArreglo():
     global nombre, edad, peso
     nombre = [""]; edad = [0]; peso = [0]
     for i in range(1, 21):
-        nombre.append = ("x")
-        edad.append = (0)
-        peso.append = ("")
+        nombre.append("")
+        edad.append(0)
+        peso.append("")
         i = i + 1
 
 def IngresoDatos():
@@ -13,37 +13,40 @@ def IngresoDatos():
     cant = int(input("Cantidad de personas: "))
     i = 0
     while i < cant:
-        nombre[i] = input("Ingrese nombre de persona", i + 1, " : ")
+        print("Ingrese nombre de persona", i + 1, ": ")
+        nombre[i] = input() 
         edad[i] = int(input("Ingrese edad: "))
-        peso[i] = int(input("Ingrese peso en kg: "))
+        peso[i] = float(input("Ingrese peso: ", "kg"))
         i = i + 1 
 
 def PromedioPesoTotal():
     global suma_peso
     suma_peso = 0; i = 0
     while i < cant:
-        suma_peso = suma_peso + edad[i]
+        suma_peso = suma_peso + peso[i]
         i = i + 1
 
 def SumaPesos100():
     global pesos100
     pesos100 = 0 ; i = 0
     while i < cant:
-        if edad[i] > 100:
-            pesos100 = pesos100 + edad[i]
+        if peso[i] > 100:
+            pesos100 = pesos100 + peso[i]
         i = i + 1
 
 DefinirArreglo()
 IngresoDatos()
 PromedioPesoTotal()
 SumaPesos100()
-print("Total de personas: ", cant)
-print("Promedio de todos los pesos: ", suma_peso/cant)
-
-if peso[i] > suma_peso/cant:
-    print("Personas con pesos mayor al promedio: ", nombre[i], edad[i], peso[i])
+print("Total de personas:", cant)
+print("Promedio de todos los pesos:", suma_peso/cant)
+i = 0
+print("Personas con pesos mayor al promedio: ") 
+while i < cant:
+    if peso[i] > suma_peso/cant:
+        print(nombre[i], "   Edad:", edad[i], "   Peso:", peso[i], "kg.")
     i = i + 1
 
-print("Suma del peso de todas las personas que pesan más de 100 kg: ", pesos100)
+print("Suma del peso de todas las personas que pesan más de 100 kg: ", pesos100, "kg.")
 
 input()
