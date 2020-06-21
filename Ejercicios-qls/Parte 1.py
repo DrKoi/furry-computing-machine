810ec567c46e286ea585b6fc274f05ae97add321
Kevin Figueroa <64949206+DrKoi@users.noreply.github.com>
2020-06-20 20:29:57 -0400

    Subiendo ejercicios

---
+++ Ejercicios-qls/Parte 1.py
@@ -0,0 +1,10 @@
+## Círculos
+from math import pi
+
+radio = int(input("Ingrese el radio del círculo y obtendrá su perímetro y área: "))
+perimetro = 2 * pi * radio
+area = radio ** 2 * pi
+print("Perímetro: ", perimetro)
+print("Área: ", area)
+
+input()
